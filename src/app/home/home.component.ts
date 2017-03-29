859b5d4a3ca967e249c4976f8b2a4b1c883c7f34
Ashvini <ashvinikumar45@gmail.com>
2017-03-29 11:32:24 +0530

    dynmically generating index number for story

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	items: number[];
 
-  constructor() { }
+  constructor() { 
+  	this.items = Array(30);
+  }
 
   ngOnInit() {
   }
